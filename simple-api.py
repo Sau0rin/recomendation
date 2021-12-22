c7421b8317e32d85a7cb56bb79fc634ff81fee18
Sau0rin <96497735+Sau0rin@users.noreply.github.com>
2021-12-22 13:13:01 +0200

    Update simple-api.py
    add search  id in site

--- simple-api.py
+++ simple-api.py
@@ -4,44 +4,7 @@ import sqlite3
 
 app = FastAPI()
 
-con = sqlite3.connect('myDatabase.db')
-cursorObj = con.cursor()
-
-
-jj = 234
-search_tovara = "SELECT prod_link FROM Cards where id = ?"
-cursorObj.execute(search_tovara, (jj,))
-
-f=''
-for row in cursorObj.fetchall():
-    f = row 
-h= " , " .join(f)
-aa= '-'.join(h.split('-')[:-1])
-
-search_first_img_tovara = "SELECT image_link FROM Cards where prod_link = ?"
-cursorObj.execute(search_first_img_tovara,(h,))
-picfer =''
-for row in cursorObj.fetchall():
-    picfer = row 
-
-search_all_tovarov = "SELECT prod_link FROM Cards where prod_link LIKE ?"
-cursorObj.execute(search_all_tovarov,(aa+'%',))
-fff=cursorObj.fetchall()
-
-
-search_all_img_tovarov = "SELECT image_link FROM Cards where prod_link LIKE ?"
-cursorObj.execute(search_all_img_tovarov,(aa+'%',))
-pic = cursorObj.fetchall()
-
-
-
-
-
-
-
-
 origins = ["*"]
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -54,8 +17,36 @@ app.add_middleware(
 
 
 @app.post("/request/")
-def request(name: int = Form(...), first: str = Form(h), img_ferst: str = Form(picfer),  ff: str = Form(fff), image: str = Form(pic)):
-    return {"username": name, "first": first, "img_ferst": img_ferst ,  "ff": ff, "image": image }
+def request(name: str = Form(...)):
+
+    con = sqlite3.connect('myDatabase.db')
+    cursorObj = con.cursor()
+
+    search_tovara = "SELECT prod_link FROM Cards where id = ?"
+    cursorObj.execute(search_tovara, (name,))
+
+    f=''
+    for row in cursorObj.fetchall():
+        f = row 
+    h= " , " .join(f)
+    aa= '-'.join(h.split('-')[:-1])
+
+    search_first_img_tovara = "SELECT image_link FROM Cards where prod_link = ?"
+    cursorObj.execute(search_first_img_tovara,(h,))
+    img_ferst =''
+    for row in cursorObj.fetchall():
+        img_ferst = row 
+
+    search_all_tovarov = "SELECT prod_link FROM Cards where prod_link LIKE ?"
+    cursorObj.execute(search_all_tovarov,(aa+'%',))
+    ff=cursorObj.fetchall()
+
+
+    search_all_img_tovarov = "SELECT image_link FROM Cards where prod_link LIKE ?"
+    cursorObj.execute(search_all_img_tovarov,(aa+'%',))
+    image = cursorObj.fetchall()
+
+    return {"username": name, "first": h, "img_ferst": img_ferst ,  "ff": ff, "image": image }
    
 
 
